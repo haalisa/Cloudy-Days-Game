f6c442ab62748d7ed05d798f82692a0ad9b30ffc
Iris <il.siris.li@gmail.com>
2019-08-16 14:58:10 -0700

    Update maze.py

--- maze.py
+++ maze.py
@@ -88,120 +88,120 @@ class Wall(object):
         walls.append(self)
         self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
 
-def mazegame():
-    # Initialise pygame
-    os.environ["SDL_VIDEO_CENTERED"] = "1"
-    pygame.init()
 
-    # Set up the display
-    pygame.display.set_caption("Get to the red square!")
-    screen = pygame.display.set_mode((1200, 800), flags)
-    # ?screen = pygame.display.set_mode((320, 240))
+# Initialise pygame
+os.environ["SDL_VIDEO_CENTERED"] = "1"
+pygame.init()
 
-    clock = pygame.time.Clock()
-    walls = [] # List to hold the walls
-    player = Cat() # Create the player
+# Set up the display
+pygame.display.set_caption("Get to the red square!")
+screen = pygame.display.set_mode((1200, 800), flags)
+# ?screen = pygame.display.set_mode((320, 240))
 
-    # Holds the level layout in a list of strings.
-    level = [
-    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
-    "W              W                                                       W",
-    "W              W                                                       W",
-    "W              W                                                       W",
-    "W   WWWWWWWW   W   WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW   W",
-    "W   W      W   W                        W                          W   W",
-    "W   W      W   W                        W                          W   W",
-    "W   W      W   W                        W                          W   W",
-    "W   W   WWWW   WWWWWWWWWWWWWWWWWWWWWW   WWWWWWWWWWW   WWWWWWWWWW   W   W",
-    "W   W   W           W               W             W   W            W   W",
-    "W   W   W           W               W             W   W            W   W",
-    "W   W   W           W               W             W   W            W   W",
-    "W   W   WWWWWWWWW   W   WWWWWWWWW   WWWWWWWWWWW   W   W   WWWWWWWWWW   W",
-    "W   W   W           W   W       W   W             W   W            W   W",
-    "W   W   W           W   W       W   W             W   W            W   W",
-    "W   W   W           W   W       W   W             W   W            W   W",
-    "W   W   W   WWWWWWWWW   W   W   W   W   WWWWWWWWWWW   WWWWWWWWWW   W   W",
-    "W   W   W               W   W   W   W                 W            W   W",
-    "W   W   W               W   W   W   W                 W            W   W",
-    "W   W   W               W   W   W   W                 W            W   W",
-    "W   W   WWWWWWWWWWWWWWWWW   W   W   WWWWWWWWWWW   W   W   WWWWWWWWWW   W",
-    "W   W                       W   W                 W   W            W   W",
-    "W   W                       W   W                 W   W            W   W",
-    "W   W                       W   W                 W   W            W   W",
-    "W   WWWWWWWWWWWWWWWWWWWWW   W   W                 W   WWWWWWWWWW   W   W",
-    "W                               W                 W            W   W   W",
-    "W                               W                 W            W   W   W",
-    "W                               W                 W            W   W   W",
-    "W   WWWWWWWWWWWWWWWWWWWWWWWWWWWWW                 WWWWWWWWWW   W   W   W",
-    "W            W                  W                          W   W   W   W",
-    "W            W                  W        E                 W   W   W   W",
-    "W            W                  W                          W   W   W   W",
-    "WWWWWWWWWW   W    WWW   WWWW    WWWWWWWWWWWWWWWWWWWW   WWWWW   W   W   W",
-    "W        W   W    WWW   WWWW    W       W   W              W   W   W   W",
-    "W        W   W    WWW   WWWW    W       W   W              W   W   W   W",
-    "W        W   W    WWW   WWWW    W       W   W              W   W   W   W",
-    "W   WW   W   W    WWW   WWWW    W   W   W   W   WWWWWWWW   W   W   W   W",
-    "W   WW   W   W    WWW   WWWW    W   W   W   W   W          W   W   W   W",
-    "W   WW   W   W    WWW   WWWW    W   W   W   W   W          W   W   W   W",
-    "W   WW   W   W    WWW   WWWW    W   W   W   W   W          W   W   W   W",
-    "W   WW   W   WWWWWWWW   WWWW    W   W   W   W   W   WWWWWWWW   W   W   W",
-    "W   WW   W              WWWW    W   W   W   W   W   W          W   W   W",
-    "W   WW   W              WWWW    W   W   W   W   W   W          W   W   W",
-    "W   WW   W              WWWW    W   W   W   W   W   W          W   W   W",
-    "W   WW   WWWWWWWWWWWWWWWWWWW    W   W   W   W   W   WWWWWWWWWWWW   W   W",
-    "W   WW                              W           W          W       W    ",
-    "W   WW                              W           W          W       W    ",
-    "W   WW                              W           W          W       W    ",
-    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
-    ]
+clock = pygame.time.Clock()
+walls = [] # List to hold the walls
+player = Cat() # Create the player
 
-    # Parse the level string above. W = wall, E = exit
-    x = y = 0
-    for row in level:
-        for col in row:
-            if col == "W":
-                Wall((x, y))
-            if col == "E":
-                end_rect = pygame.Rect(x, y, 16, 16)
-            x += 16
-        y += 16
-        x = 0
+# Holds the level layout in a list of strings.
+level = [
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"W              W                                                       W",
+"W              W                                                       W",
+"W              W                                                       W",
+"W   WWWWWWWW   W   WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW   W",
+"W   W      W   W                        W                          W   W",
+"W   W      W   W                        W                          W   W",
+"W   W      W   W                        W                          W   W",
+"W   W   WWWW   WWWWWWWWWWWWWWWWWWWWWW   WWWWWWWWWWW   WWWWWWWWWW   W   W",
+"W   W   W           W               W             W   W            W   W",
+"W   W   W           W               W             W   W            W   W",
+"W   W   W           W               W             W   W            W   W",
+"W   W   WWWWWWWWW   W   WWWWWWWWW   WWWWWWWWWWW   W   W   WWWWWWWWWW   W",
+"W   W   W           W   W       W   W             W   W            W   W",
+"W   W   W           W   W       W   W             W   W            W   W",
+"W   W   W           W   W       W   W             W   W            W   W",
+"W   W   W   WWWWWWWWW   W   W   W   W   WWWWWWWWWWW   WWWWWWWWWW   W   W",
+"W   W   W               W   W   W   W                 W            W   W",
+"W   W   W               W   W   W   W                 W            W   W",
+"W   W   W               W   W   W   W                 W            W   W",
+"W   W   WWWWWWWWWWWWWWWWW   W   W   WWWWWWWWWWW   W   W   WWWWWWWWWW   W",
+"W   W                       W   W                 W   W            W   W",
+"W   W                       W   W                 W   W            W   W",
+"W   W                       W   W                 W   W            W   W",
+"W   WWWWWWWWWWWWWWWWWWWWW   W   W                 W   WWWWWWWWWW   W   W",
+"W                               W                 W            W   W   W",
+"W                               W                 W            W   W   W",
+"W                               W                 W            W   W   W",
+"W   WWWWWWWWWWWWWWWWWWWWWWWWWWWWW                 WWWWWWWWWW   W   W   W",
+"W            W                  W                          W   W   W   W",
+"W            W                  W        E                 W   W   W   W",
+"W            W                  W                          W   W   W   W",
+"WWWWWWWWWW   W    WWW   WWWW    WWWWWWWWWWWWWWWWWWWW   WWWWW   W   W   W",
+"W        W   W    WWW   WWWW    W       W   W              W   W   W   W",
+"W        W   W    WWW   WWWW    W       W   W              W   W   W   W",
+"W        W   W    WWW   WWWW    W       W   W              W   W   W   W",
+"W   WW   W   W    WWW   WWWW    W   W   W   W   WWWWWWWW   W   W   W   W",
+"W   WW   W   W    WWW   WWWW    W   W   W   W   W          W   W   W   W",
+"W   WW   W   W    WWW   WWWW    W   W   W   W   W          W   W   W   W",
+"W   WW   W   W    WWW   WWWW    W   W   W   W   W          W   W   W   W",
+"W   WW   W   WWWWWWWW   WWWW    W   W   W   W   W   WWWWWWWW   W   W   W",
+"W   WW   W              WWWW    W   W   W   W   W   W          W   W   W",
+"W   WW   W              WWWW    W   W   W   W   W   W          W   W   W",
+"W   WW   W              WWWW    W   W   W   W   W   W          W   W   W",
+"W   WW   WWWWWWWWWWWWWWWWWWW    W   W   W   W   W   WWWWWWWWWWWW   W   W",
+"W   WW                              W           W          W       W    ",
+"W   WW                              W           W          W       W    ",
+"W   WW                              W           W          W       W    ",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+]
 
-    running = True
-    while running:
+# Parse the level string above. W = wall, E = exit
+x = y = 0
+for row in level:
+    for col in row:
+        if col == "W":
+            Wall((x, y))
+        if col == "E":
+            end_rect = pygame.Rect(x, y, 16, 16)
+        x += 16
+    y += 16
+    x = 0
 
-        clock.tick(60)
+running = True
+while running:
 
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                running = False
-            if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
-                running = False
+    clock.tick(60)
 
-        # Move the player if an arrow key is pressed
-        key = pygame.key.get_pressed()
-        if key[pygame.K_LEFT]:
-            player.move(-2, 0)
-        if key[pygame.K_RIGHT]:
-            player.move(2, 0)
-        if key[pygame.K_UP]:
-            player.move(0, -2)
-        if key[pygame.K_DOWN]:
-            player.move(0, 2)
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            running = False
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+            running = False
 
-        # Just added this to make it slightly fun ;)
-        # if player.rect.colliderect(end_rect):
-        #     raise SystemExit, "You win!"
+    # Move the player if an arrow key is pressed
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        player.move(-3, 0)
+    if key[pygame.K_RIGHT]:
+        player.move(3, 0)
+    if key[pygame.K_UP]:
+        player.move(0, -3)
+    if key[pygame.K_DOWN]:
+        player.move(0, 3)
 
-        # making cat move
-        pressed_keys = pygame.key.get_pressed()
-        player.update(pressed_keys)
+    # Just added this to make it slightly fun ;)
+    # if player.rect.colliderect(end_rect):
+    #     raise SystemExit, "You win!"
 
-        # Draw the scene
-        screen.fill((0, 0, 0))
-        for wall in walls:
-            pygame.draw.rect(screen, (255, 255, 255), wall.rect)
-        pygame.draw.rect(screen, (255, 0, 0), end_rect)
-        # pygame.draw.rect(screen, (255, 200, 0), player.rect)
-        screen.blit(player.image, player.rect)
-        pygame.display.flip()
+    # making cat move
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    # Draw the scene
+    screen.fill((0, 0, 0))
+    for wall in walls:
+        pygame.draw.rect(screen, (255, 255, 255), wall.rect)
+    pygame.draw.rect(screen, (255, 0, 0), end_rect)
+    # pygame.draw.rect(screen, (255, 200, 0), player.rect)
+    screen.blit(player.image, player.rect)
+    pygame.display.flip()
