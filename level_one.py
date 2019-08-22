1efaa8eb7ca44033fb086723b9bf0d48d33889c2
irisli007 <il.siris.li@gmail.com>
2019-08-22 01:05:35 -0700

    level 1

--- level_one.py
+++ level_one.py
@@ -6,7 +6,7 @@ import pygame
 from pygame.locals import *
 
 import random
-# import level_two
+import level_two
 
 # import start_menu
 from movement import Cat
@@ -58,39 +58,24 @@ def levelone ():
 
     bed_empty = pygame.image.load("pics/lvl-bgs/full_bedroom_empty.png").convert()
     bed_empty = pygame.transform.scale(bed_empty, (width,height))
-
+    bed_ = pygame.image.load("pics/lvl-bgs/full_bedroom3.png").convert()
+    bed_ = pygame.transform.scale(bed_, (width,height))
     bp= pygame.image.load("pics/backpackgif.gif").convert()
 
     # text boxes
     speechText = pygame.font.Font('fonts/livvic/livvic-medium.ttf', 20)
-    #
-    # introtext= pygame.image.load("text/intro.png").convert()
-    # wakeup= pygame.image.load("text/wakeup.png").convert()
-    # fivemin= pygame.image.load("text/5min.png").convert()
-    # gettingup= pygame.image.load("text/gettingup.png").convert()
-    # readyy= pygame.image.load("text/readyy.png").convert()
-    # pawsitive= pygame.image.load("text/pawsitive!.png").convert()
-    # noenergy =pygame.image.load("text/kai speaking.png").convert()
-    #
-    # TextSurf4, TextRect4 = text_objects("Press enter to continue", speechText)
-    # TextRect4.center = (900,80)
-    # TextSurf3, TextRect3 = text_objects("Pick up Kai's bag using the space bar", speechText)
-    # TextRect3.center = (900,80)
-    # TextSurf1, TextRect1 = text_objects("Use left, right, and up arrows to jump on Kai's bed", speechText)
-    # TextRect1.center = (900,80)
-    # TextSurf5, TextRect5 = text_objects("Exit the room using the enter key", speechText)
-    # TextRect5.center = (900,80)
+
 
     dialogue = pygame.font.Font('fonts/livvic/livvic-medium.ttf', 20)
 
-    TextSurf_n, TextRect_n = text_objects2("Press enter to continue.", dialogue)
-    TextRect_n.center = (275,80)
+    TextSurf_n, TextRect_n = text_objects("Press enter to continue.", dialogue)
+    TextRect_n.center = (900,560)
 
-    TextSurf_n1, TextRect_n1 = text_objects2("Hi, I'm Kit Kat. That's Kai who's my best friend.", dialogue)
+    TextSurf_n1, TextRect_n1 = text_objects2("Hi, I'm Kit Kat. That's Kai, my best friend.", dialogue)
     TextRect_n1.center = (375,80)
 
-    TextSurf_n2, TextRect_n2 = text_objects2("Sometimes, Kai has trouble getting out of bed. Help me wake him up.", dialogue)
-    TextRect_n2.center = (480,80)
+    TextSurf_n2, TextRect_n2 = text_objects2("Sometimes, Kai has trouble getting out of bed. Help me wake him up. Use the left, right and up arrows.", dialogue)
+    TextRect_n2.center = (580,80)
 
     TextSurf_n3, TextRect_n3 = text_objects2("Wake up... Kai...", dialogue)
     TextRect_n3.center = (270,80)
@@ -113,18 +98,24 @@ def levelone ():
     TextSurf_n9, TextRect_n9 = text_objects2("I don't have the energy to deal with school today.", dialogue)
     TextRect_n9.center = (width/2,height/2)
 
-    TextSurf_n10, TextRect_n10 = text_objects2("Stay pawsitive, Kai!", dialogue)
+    TextSurf_n10, TextRect_n10 = text_objects2("Stay paw-sitive, Kai!", dialogue)
     TextRect_n10.center = (width/2,height/2)
 
-    TextSurf_n11, TextRect_n11 = text_objects2("I'll be by your side; it'll be a good day!", dialogue)
+    TextSurf_n11, TextRect_n11 = text_objects2("I'll be by your side, it'll be a good day!", dialogue)
     TextRect_n11.center = (width/2,height/2)
+
+    TextSurf_n12, TextRect_n12 = text_objects2("I should go grab Kai's backpack for them", dialogue)
+    TextRect_n12.center = (289,80)
+
+    TextSurf_n13, TextRect_n13 = text_objects2("Let's head out! hit enter at the door", dialogue)
+    TextRect_n13.center = (289,80)
 
     text_box = pygame.image.load("pics/teal_rect.png")
     text_box.set_alpha(200)
     text_box = pygame.transform.scale(text_box, (width,150))
 
     text_box_big = pygame.image.load("pics/teal_rect.png")
-    text_box_big.set_alpha(200)
+    # text_box_big.set_alpha(200)
     text_box_big = pygame.transform.scale(text_box, (width,height))
 
     kaicon = pygame.image.load("KAI/uh2.png")
@@ -142,14 +133,13 @@ def levelone ():
         # if kai is woken up
         if DoorOpen == True:
             screen.blit(bed_empty, (0,0))
+        if next==11:
+            screen.blit(bed_,(0,0))
         #for the backpack
         if not pickedUp:
             screen.blit(bp, (900,400))
         else:
             screen.blit(bp, (player.rect.x+110,player.rect.y+90))
-
-        if player.rect.x >800 and player.rect.x< 900:
-            pickedUp= True
 
         player.update()
 
@@ -160,14 +150,15 @@ def levelone ():
         if key[pygame.K_RIGHT]:
             player.move(2)
             player.image = pygame.image.load("KIT_KAT/kitkat_right2.png")
-        if enter >= 2:
+        if enter >= 1:
             if key[pygame.K_UP]:
                 player.jump()
 
+        screen.blit(player.image, player.rect)
 
         # cat leaves bedroom to go to kitchen
         # TO DO: make the bed wall not work in the kitchen
-        if key[pygame.K_RETURN]:
+        if key[pygame.K_RETURN] and next==11:
             if player.rect.x >900 and player.rect.x <1100:
                 truevar = False
                 level_two.leveltwo() #change to level_two later when those files have been combined
@@ -185,67 +176,79 @@ def levelone ():
 
         if enter == 1 and jumps<10:
             next = 1
-        if enter == 2:
-            next = 2
         if jumps >=1 and jumps < 5:
+            next = 2
+        if jumps >= 5 and jumps <9:
             next = 3
-        if jumps >= 5 and jumps <10:
+        if jumps >= 9 and jumps< 13:
             next = 4
-        if jumps >= 10 and jumps< 15:
+        if jumps == 13:
             next = 5
-        if jumps == 17:
-            next = 6
             DoorOpen = True
-            enter = 3
-        if jumps > 18:
-            next = 7 # press enter to continue
-        if jumps > 19 and enter == 4:
+            enter = 1
+
+        if enter==2:
             KaiUp = True
-            next = 8
+            next = 6 # press enter to continue
         if KaiUp == True:
+            if enter == 3:
+                next = 7
+            if enter == 4:
+                next = 8
             if enter == 5:
                 next = 9
             if enter == 6:
                 next = 10
-            if enter == 7:
-                next = 11
-
-        print("enters:")
-        print(enter)
-        print("jumps:")
-        print(jumps)
+                KaiUp= False
 
         if next == 0:
             screen.blit(TextSurf_n,TextRect_n)
-        if next == 1:
             screen.blit(TextSurf_n1,TextRect_n1)
-        if next == 2:
+        if next == 1:
             screen.blit(TextSurf_n2,TextRect_n2)
-        if next == 3:
+        if next == 2:
             screen.blit(TextSurf_n3,TextRect_n3)
-        if next == 4:
+        if next == 3:
             screen.blit(TextSurf_n4,TextRect_n4)
             screen.blit(kaicon, (25,15))
-        if next == 5:
+        if next == 4:
             screen.blit(TextSurf_n5,TextRect_n5)
-        if next == 6:
+        if next == 5:
             screen.blit(TextSurf_n6,TextRect_n6)
             screen.blit(kaicon, (25,15))
-        if next == 7:
-            screen.blit(TextSurf_n7,TextRect_n7)
+            screen.blit(TextSurf_n,TextRect_n)
 
         if KaiUp == True:
             #full screen text box
             screen.blit(text_box_big,(0,0))
+            screen.blit(text_box_big,(0,0))
+            if next == 6:
+                screen.blit(TextSurf_n8,TextRect_n8) #Are you ready for school?
+                screen.blit(TextSurf_n,TextRect_n)
+
+            if next == 7:
+                screen.blit(TextSurf_n9,TextRect_n9) #I don't have the energy to deal with school today
+                screen.blit(TextSurf_n,TextRect_n)
+                screen.blit(kaicon, (260,290))
+
+
             if next == 8:
-                screen.blit(text_box_big,(0,0))
-                screen.blit(TextSurf_n8,TextRect_n8)
+                screen.blit(TextSurf_n10,TextRect_n10) #stay pawsitive
+                screen.blit(TextSurf_n,TextRect_n)
             if next == 9:
-                screen.blit(TextSurf_n9,TextRect_n9)
-            if next == 10:
-                screen.blit(TextSurf_n10,TextRect_n10)
-            if next == 11:
-                screen.blit(TextSurf_n11,TextRect_n11)
+                screen.blit(TextSurf_n11,TextRect_n11) #I'll be by your side
+                screen.blit(TextSurf_n,TextRect_n)
+        if next == 10:
+            screen.blit(TextSurf_n12,TextRect_n12) #backpack
+            if player.rect.x >800 and player.rect.x< 900:
+                pickedUp= True
+                next = 11
+                print ("true")
+        if next == 11:
+            screen.blit(TextSurf_n13,TextRect_n13) #let's go
+
+
+
 
         pygame.display.flip()
 
